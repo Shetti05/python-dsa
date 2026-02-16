8cf48ba92b9c6830fad4c2522c114b993f184bd0
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-16 20:50:04 +0530

    feb16/3

---
+++ feb16/trie_implementation.py
@@ -0,0 +1,22 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            node = node.children.setdefault(char, TrieNode())
+        node.end = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.end
