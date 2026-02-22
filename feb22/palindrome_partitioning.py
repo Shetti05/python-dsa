43528e0195b514e6fc483055c3f510cad09a2444
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-22 20:27:07 +0530

    commit 2

---
+++ feb22/palindrome_partitioning.py
@@ -0,0 +1,14 @@
+def partition(s):
+    result = []
+
+    def backtrack(start, path):
+        if start == len(s):
+            result.append(path[:])
+            return
+        for end in range(start+1, len(s)+1):
+            substring = s[start:end]
+            if substring == substring[::-1]:
+                backtrack(end, path + [substring])
+
+    backtrack(0, [])
+    return result
