5fc7a15dfcbcb7b6670736cd1234ca02dc76eea9
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-17 21:11:40 +0530

    commit 6

---
+++ mar17/cycle_detection.py
@@ -0,0 +1,18 @@
+def has_cycle(graph):
+    visited = set()
+
+    def dfs(node, parent):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                if dfs(neighbor, node):
+                    return True
+            elif neighbor != parent:
+                return True
+        return False
+
+    for node in graph:
+        if node not in visited:
+            if dfs(node, -1):
+                return True
+    return False
