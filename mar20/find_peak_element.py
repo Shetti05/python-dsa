b6b0d0712d091c2834dc468026e8799575e4dc48
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-20 18:35:15 +0530

    commit 23

---
+++ mar20/find_peak_element.py
@@ -0,0 +1,13 @@
+def findPeak(nums):
+    left, right = 0, len(nums)-1
+    
+    while left < right:
+        mid = (left + right)//2
+        if nums[mid] > nums[mid+1]:
+            right = mid
+        else:
+            left = mid + 1
+            
+    return left
+
+print(findPeak([1,2,1,3,5,6,4]))
