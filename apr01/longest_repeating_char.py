89066280cfbd7c7750d0579c2da64546bc7af32d
Jaideep <jaideepshetti55@gmail.com>
2026-04-01 20:17:25 +0530

    commit 2

---
+++ apr01/longest_repeating_char.py
@@ -0,0 +1,19 @@
+def character_replacement(s, k):
+    count = {}
+    left = 0
+    maxf = 0
+    res = 0
+
+    for right in range(len(s)):
+        count[s[right]] = 1 + count.get(s[right], 0)
+        maxf = max(maxf, count[s[right]])
+
+        while (right - left + 1) - maxf > k:
+            count[s[left]] -= 1
+            left += 1
+
+        res = max(res, right - left + 1)
+
+    return res
+
+print(character_replacement("AABABBA", 1))
